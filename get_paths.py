e15d2ed44d162dee1684c425fb5d00b7dea25178
Joris Gillis <joris.gillis42@gmail.com>
2021-12-22 10:48:17 +0100

    work

---
+++ get_paths.py
@@ -0,0 +1,8 @@
+from sysconfig import get_paths
+
+import os
+import json
+d=get_paths()
+d['libs']=os.path.join(os.environ['pythonLocation'],'libs')
+for k,v in d.items()
+  print('::set-output '+k+'=paths::'+v)
